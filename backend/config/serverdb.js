105627891a6f5f6db091d4204de3f99dab95c51b
pankaj-msl <pankaj.pandey@msllearning.com>
2024-11-30 11:41:04 +0530

    server setup

--- backend/config/serverdb.js
+++ backend/config/serverdb.js
@@ -1,16 +1,27 @@
-// db.js
-const mysql = require('mysql');
+//serverdb.js
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'pankaj',
-  password: 'Pankaj@616',
-  database: 'u18chatbot'
+const db = mysql.createPool({
+  host: process.env.SERVER_DB_HOST,
+  port: 3306,
+  user: process.env.SERVER_DB_USERNAME,
+  password: process.env.SERVER_DB_PASSWORD,
+  database:  process.env.SERVER_DB
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL');
-});
+// Optional: Test the connection
+const testConnection = async () => {
+    try {
+        const connection = await db.getConnection();
+        console.log('MySQL Database connected successfully');
+        connection.release();
+    } catch (error) {
+        console.error('Error connecting to the database:', error.message);
+        process.exit(1); // Exit the process with an error code
+    }
+};
 
-module.exports = db;
+// Test connection when the module is imported
+testConnection();
+module.exports = { db };
